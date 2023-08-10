c28e14eeb48c1219851b4a2fdb33e5c1f264dbd9
JasubiPL <gameportal7@hotmail.com>
2023-08-10 12:34:43 -0600

    Added change 10-08-23

--- Drivers_Logbook/js/status-checks.mjs
+++ Drivers_Logbook/js/status-checks.mjs
@@ -4,23 +4,23 @@ const btnAuxiliary = document.getElementById("btn-auxiliary")
 const btnExceptions = document.getElementById("btn-exceptions")
 const btnBreaks = document.getElementById("btn-breaks")
 const btnOutService = document.getElementById("btn-outService")
+const btnAddReason = document.getElementById("btn-add-reason")
 
 const noteOptions = document.getElementById("note-options")
 const pauseOptions = document.getElementById("pause-options")
 const exceptionsOptions = document.getElementById("exceptions-options")
 const auxiliaryOptions = document.getElementById("auxiliary-options")
-const formDate = document.getElementById(".status-form-date")
+const formDate = document.getElementById("status-form-date")
 const statusTitle = document.getElementById("status-title")
 
 const statusModal = document.querySelector(".status")
+const reasonModal = document.querySelector(".add-reason-modal")
 const btnControls = document.querySelectorAll(".driver-menu-icon")
 const formOptions = document.querySelectorAll(".status-form-options")
+const btnShowAddReasonModal = document.querySelectorAll(".add-reason")
 
 
-const btnReturn = document.querySelector(".return-icon").addEventListener("click", () =>{
-  statusModal.style.transform = "translateX(-100vw)"
-  confirm("¿Salir sin guardar cambios?")
-})
+const btnReturn = document.querySelector(".return-icon").addEventListener("click", returnStatus)
 
 const btnSave = document.querySelector(".save-icon").addEventListener("click", saveStatus)
 
@@ -30,6 +30,11 @@ btnAuxiliary.addEventListener("click", activeAuxiliary)
 btnExceptions.addEventListener("click", activeExceptions)
 btnBreaks.addEventListener("click", activeBreaks)
 btnOutService.addEventListener("click", activeOutService)
+btnAddReason.addEventListener("click", saveReason)
+
+btnShowAddReasonModal.forEach(btn =>{
+  btn.addEventListener("click", ShowAddReasonModal)
+})
 
 
 function activeDriving(){
@@ -63,7 +68,7 @@ function activePause(){
   btnPause.classList.add("active")
   statusModal.style.transform = "translateX(0)"
   statusTitle.innerText = "Pausa"
-  pauseOptions.style.display = "block"
+  pauseOptions.style.display = "flex"
   formDate.style.display = "block"
 }
 
@@ -80,7 +85,7 @@ function activeAuxiliary(){
   btnAuxiliary.classList.add("active")
   statusModal.style.transform = "translateX(0)"
   statusTitle.innerText = "Actividades Auxiliares"
-  auxiliaryOptions.style.display = "block"
+  auxiliaryOptions.style.display = "flex"
   formDate.style.display = "none"
 }
 
@@ -97,7 +102,7 @@ function activeExceptions(){
   btnExceptions.classList.add("active")
   statusModal.style.transform = "translateX(0)"
   statusTitle.innerText = "Casos de Excepcion"
-  exceptionsOptions.style.display = "block"
+  exceptionsOptions.style.display = "flex"
   formDate.style.display = "none"
 }
 
@@ -115,7 +120,7 @@ function activeBreaks(){
   statusModal.style.transform = "translateX(0)"
   statusTitle.innerText = "Descanso"
   noteOptions.style.display = "block"
-  formDate.style.display = "none"
+  formDate.style.display = "block"
 }
 
 function activeOutService(){
@@ -136,6 +141,51 @@ function activeOutService(){
 }
 
 function saveStatus(){
+  let reasonValue = document.getElementById("reason-value")
+  const newReason = document.querySelectorAll(".new-reason")
+
   confirm("¿Está seguro de grabar los cambios?")
   statusModal.style.transform = "translateX(-100vw)"
+
+  reasonModal.style.display = "none"
+  reasonValue.value = ""
+  newReason.forEach(reason =>{
+    reason.style.display = "none"
+    reason.innerText = ""
+  })
+
+  
+}
+
+function returnStatus(){
+  let reasonValue = document.getElementById("reason-value")
+  const newReason = document.querySelectorAll(".new-reason")
+
+  statusModal.style.transform = "translateX(-100vw)"
+  confirm("¿Salir sin guardar cambios?")
+
+  reasonModal.style.display = "none"
+  reasonValue.value = ""
+  newReason.forEach(reason =>{
+    reason.style.display = "none"
+    reason.innerText = ""
+  })
+}
+
+function ShowAddReasonModal(e){
+  e.preventDefault()
+  reasonModal.style.display = "flex"
+}
+
+function saveReason(){
+  let reasonValue = document.getElementById("reason-value").value
+  const newReason = document.querySelectorAll(".new-reason")
+
+  reasonModal.style.display = "none"
+  newReason.forEach(reason =>{
+    reason.style.display = "block"
+    reason.innerText = reasonValue
+  })
+
+
 }
