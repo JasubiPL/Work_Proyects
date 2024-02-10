d88701733c76f62750ece3c269dd40d10fd2317c
Jasubi Piñeyro <gameportal7@hotmail.com>
2024-02-10 11:04:26 -0600

    Se creo el Login

--- dashboard_digital_signage/src/router/AppRouter.tsx
+++ dashboard_digital_signage/src/router/AppRouter.tsx
@@ -2,12 +2,14 @@ import { Navigate, Route, Routes } from "react-router-dom"
 import { AdminRoutes } from "../users/admin/routes/AdminRoutes"
 import { ManagerRoutes } from "../users/manager/routes/ManagerRutes"
 import { DesignersRoutes } from "../users/designers/routes/DesignersRoutes"
+import { LoginPage } from "../pages"
 
 export const AppRouter = () =>{
   return(
     <Routes>
       <Route path="/*" element={<Navigate to='err404' />} />
-      <Route path="/" element={<Navigate to='admin' />} />
+      <Route path="/" element={<Navigate to='login' />} />
+      <Route path="/login" element={<LoginPage />} />
       <Route path="admin/*" element={<AdminRoutes />} />
       <Route path="manager/*" element={<ManagerRoutes />} />
       <Route path="designers/*" element={<DesignersRoutes />} />
