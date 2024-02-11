d03e49001ec35fe2ff74b0d90f6dc6c8dd970936
Jasubi Piñeyro <gameportal7@hotmail.com>
2024-02-11 10:37:06 -0600

    Se agrego la primera validacion para el login con el redireccionamiento

--- dashboard_digital_signage/src/pages/LoginPage.tsx
+++ dashboard_digital_signage/src/pages/LoginPage.tsx
@@ -1,7 +1,32 @@
+import { FormEvent, useEffect, useState } from "react"
+import { login } from "../auth/auth"
+import { useNavigate } from "react-router-dom";
+
 export const LoginPage = () =>{
+  const [emailInput, setEmailInput] = useState("")
+  const navigate = useNavigate()
+
+  const handlerLogin = (e: FormEvent) =>{
+    e.preventDefault()
+
+    const resp = login(emailInput)
+
+    if(resp.status === 200){
+      const user = resp.data
+      return navigate(`/${user?.area}/dashboard`)
+    }else{
+      alert("Usuario no encontrado")
+    }
+  }
+
+  useEffect(() =>{
+    
+  },[])
   return (
     <section className="w-full h-screen bg-gray-100 flex flx-col justify-center items-center">
-      <form className="w-full max-w-sm lg:max-w-md mx-auto bg-white flex flex-col shadow-xl p-8">
+      <form
+        onSubmit={handlerLogin}
+        className="w-full max-w-sm lg:max-w-md mx-auto bg-white flex flex-col shadow-xl p-8">
         <div className="w-full flex flex-col items-center mb-8">
           <img className="w-1/3 self-center" src="/grupo_iamsa_logo.jpg" alt="logo grupo iamsa" />
           <span className="text-2xl font-semibold text-red-600 ">Señalizacion Digital</span>
@@ -14,14 +39,17 @@ export const LoginPage = () =>{
               <path d="M11.241 9.817c-.36.275-.801.425-1.255.427-.428 0-.845-.138-1.187-.395L0 2.6V14a2 2 0 0 0 2 2h16a2 2 0 0 0 2-2V2.5l-8.759 7.317Z"/>
             </svg>
           </div>
-          <input 
+          <input
             type="text" 
             id="email-address-icon" 
             className="bord text-sm block w-full ps-10 p-2.5 border-b-2
             border-red-600 placeholder-red-400 text-whitefocus:border-blue-500" 
-            placeholder="name@grupo-iamsa.com.mx" />
+            placeholder="name@grupo-iamsa.com.mx" 
+            onChange={(e) => setEmailInput(e.target.value) }
+          />
         </div>
-        <button className=" py-2 px-4 bg-red-600 text-white max-w-24 self-end mt-8">
+        <button 
+          className=" py-2 px-4 bg-red-600 text-white max-w-24 self-end mt-8">
           Acceder
         </button>
       </form>
