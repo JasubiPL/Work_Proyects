c2d0f711a987c9e123b2ff083f33a66d3a34b85c
Jasubi Piñeyro <gameportal7@hotmail.com>
2024-02-17 17:26:43 -0600

    API_dashboard_DS: Se usa multer para guardar procesar el archivo, aun falta ponerle nombre y extencion

--- API_dashboard_DS/index.js
+++ API_dashboard_DS/index.js
@@ -1,8 +1,11 @@
 import express from "express"
 import cors from "cors"
+import multer from "multer"
 
 const PORT = process.env.PORT || 7000
+const blueprintsFolder = multer({dest: "blueprints/"})
 const app = express()
+
 
 app.use(cors({origin: "*"}))
 app.use(express.json());
@@ -11,8 +14,15 @@ app.get("/", (req, res) =>{
   res.json({"Ruta no valida": "Mira al documentacion de la API en https://jasubip.vercel.app"})
 })
 
-app.post("/api", (req, res) =>{
+app.get("/api",  (req, res, next) =>{
+  res.send("<h1>Bienvenido a la API</h1>")
+
+  res.json({"message": "conected"})
+})
+
+app.post("/api", blueprintsFolder.single("document"), (req, res, next) =>{
   console.log(req.body)
+  console.log(req.file)
 
   res.json({"message": "conected"})
 })
