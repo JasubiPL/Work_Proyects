a334e8832c4f59ad7f654363eaedc5dbb6c15394
Jasubi Piñeyro <gameportal7@hotmail.com>
2024-02-28 11:56:01 -0600

    [DS_Dashboard]: Se añade la parte de software, queda pendiente dashboard y Documentacion

--- dashboard_digital_signage/src/pages/DocumentationPage.tsx
+++ dashboard_digital_signage/src/pages/DocumentationPage.tsx
@@ -3,7 +3,7 @@
 export const DocumentationPage = () =>{
   return(
     <section className="w-full  h-full">
-      <h1>Documentacion</h1>
+      <h1>Priximamente...</h1>
     </section>
   )
 }
--- dashboard_digital_signage/src/users/admin/components/index.ts
+++ dashboard_digital_signage/src/users/admin/components/index.ts
@@ -1,2 +1,3 @@
 export * from './NavbarAdmin'
 export * from './UploadFilesForm'
+export * from './SoftwareItem'
